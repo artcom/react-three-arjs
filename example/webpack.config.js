7af8a029faec275ba51c9d5e99958abccbb6b561
Jens Eraßmy <jens.erassmy@artcom.de>
2021-09-21 15:21:46 +0200

    Update example

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -3,26 +3,22 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
-const webpack = require("webpack")
 
 module.exports = (_, { mode }) => ({
   devServer: {
-    publicPath: "/",
-    contentBase: "./dist",
     host: "0.0.0.0",
-    https: true
+    hot: true
   },
   plugins: [
-    mode === "development" && new webpack.HotModuleReplacementPlugin(),
     mode === "development" && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "src/index.html",
       inject: false
     }),
-    new CopyWebpackPlugin([
-      { from: "lib/", to: "lib/" },
-      { from: "data/", to: "data/" }
-    ])
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "data/", to: "data/" },
+      ] }),
   ].filter(Boolean),
   devtool: mode === "development" ? "eval-source-map" : "source-map",
   module: {
