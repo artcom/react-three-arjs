8e51982356e0e41d76e33e872027f85834cf4c68
Jens Eraßmy <jens.erassmy@artcom.de>
2021-11-15 13:24:46 +0100

    Update example

--- example/src/index.js
+++ example/src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom"
 import React from "react"
 
 import { ARCanvas, ARMarker } from "@artcom/react-three-arjs"
+import { sRGBEncoding } from "three"
 
 function Box() {
   return (
@@ -19,15 +20,16 @@ ReactDOM.render(
     onCameraStreamReady={ () => console.log("Camera stream ready") }
     onCameraStreamError={ () => console.error("Camera stream error") }
     onCreated={ ({ gl }) => {
-      gl.outputEncoding = THREE.sRGBEncoding
+      gl.outputEncoding = sRGBEncoding
       gl.physicallyCorrectLights = true
       gl.setSize(window.innerWidth, window.innerHeight)
     } }>
     <ambientLight />
     <pointLight position={ [10, 10, 0] } intensity={ 10.0 } />
     <ARMarker
+      params={ { smooth: true } }
       type={ "pattern" }
-      patternUrl={ "data/hiro.patt" }
+      patternUrl={ "data/patt.hiro" }
       onMarkerFound={ () => { console.log("Marker Found")} }>
       <Box />
     </ARMarker>
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,10 +1,12 @@
 /* eslint-disable import/no-commonjs */
-const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = (_, { mode }) => ({
+  entry: {
+    main: "./src/index.js",
+  },
   devServer: {
     host: "0.0.0.0",
     hot: true
@@ -12,12 +14,11 @@ module.exports = (_, { mode }) => ({
   plugins: [
     mode === "development" && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "src/index.html",
-      inject: false
+      template: "src/index.html"
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: "data/", to: "data/" },
+        { from: "./node_modules/@ar-js-org/ar.js/data/data/", to: "data/" },
       ] }),
   ].filter(Boolean),
   devtool: mode === "development" ? "eval-source-map" : "source-map",
@@ -38,23 +39,5 @@ module.exports = (_, { mode }) => ({
         ]
       }
     ]
-  },
-  entry: {
-    app: {
-      import: "./src/index.js",
-      dependOn: "shared",
-    },
-    threeJS: {
-      import: "./src/threeJSEntry.js",
-      dependOn: "shared",
-    },
-    shared: "three"
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  optimization: {
-    runtimeChunk: "single"
-  },
+  }
 })
