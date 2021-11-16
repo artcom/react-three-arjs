9db7186c2d46c1093471014ecea50ce7ecded9d7
Jens Eraßmy <jens.erassmy@artcom.de>
2021-11-16 10:56:54 +0100

    Update example

--- example/src/index.js
+++ example/src/index.js
@@ -27,7 +27,6 @@ ReactDOM.render(
     <ambientLight />
     <pointLight position={ [10, 10, 0] } intensity={ 10.0 } />
     <ARMarker
-      params={ { smooth: true } }
       type={ "pattern" }
       patternUrl={ "data/patt.hiro" }
       onMarkerFound={ () => { console.log("Marker Found")} }>
