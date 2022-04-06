8f7a445400e8c9ae8915430b7b83cc184ff6f32c
Jens Eraßmy <jens.erassmy@artcom.de>
2022-04-06 12:50:02 +0200

    Refactor

--- src/index.d.ts
+++ src/index.d.ts
@@ -21,6 +21,6 @@ export interface ARMarkerProps extends React.PropsWithChildren {
   onMarkerLost: () => void;
 }
 
-export declare function ARCanvas({ arEnabled, tracking, patternRatio, detectionMode, cameraParametersUrl, matrixCodeType, sourceType, onCameraStreamReady, onCameraStreamError, ...rest }: ARCanvasProps): JSX.Element;
+export declare function ARCanvas(props: ARCanvasProps): JSX.Element;
 
-export declare function ARMarker({ type, barcodeValue, patternUrl, params, onMarkerFound, onMarkerLost}: ARMarkerProps): JSX.Element;
+export declare function ARMarker(props: ARMarkerProps): JSX.Element;
