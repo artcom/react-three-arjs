e1594aa0ccedfd7f2520df9dc61164858abe8392
Jens Eraßmy <jens.erassmy@artcom.de>
2022-04-11 16:44:34 +0200

    Format using prettier

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,4 +1,5 @@
-/* eslint-disable import/no-commonjs */
+/* eslint-disable no-undef */
+
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
@@ -10,29 +11,29 @@ module.exports = (_, { mode }) => ({
   },
   devServer: {
     host: "0.0.0.0",
-    hot: true
+    hot: true,
   },
   resolve: {
     alias: {
-      "react": path.resolve("./node_modules/react"),
+      react: path.resolve("./node_modules/react"),
       "react-dom": path.resolve("./node_modules/react-dom"),
       "react-redux": path.resolve("./node_modules/react-redux"),
-      "three": path.resolve("./node_modules/three"),
+      three: path.resolve("./node_modules/three"),
       "styled-components": path.resolve("./node_modules/styled-components"),
-      "@react-three/fiber": path.resolve("./node_modules/@react-three/fiber")
-    }
+      "@react-three/fiber": path.resolve("./node_modules/@react-three/fiber"),
+    },
   },
   plugins: [
     mode === "development" && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "src/index.html"
+      template: "src/index.html",
     }),
     new CopyWebpackPlugin({
       patterns: [
         { from: "./node_modules/@ar-js-org/artoolkit5-js/data/", to: "data/" },
         { from: "./data/", to: "data/" },
-      ] }),
-
+      ],
+    }),
   ].filter(Boolean),
   devtool: mode === "development" ? "eval-source-map" : "source-map",
   module: {
@@ -44,13 +45,11 @@ module.exports = (_, { mode }) => ({
           {
             loader: "babel-loader",
             options: {
-              plugins: [
-                mode === "development" && "react-refresh/babel",
-              ].filter(Boolean),
+              plugins: [mode === "development" && "react-refresh/babel"].filter(Boolean),
             },
           },
-        ]
-      }
-    ]
-  }
+        ],
+      },
+    ],
+  },
 })
