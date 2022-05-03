951e6b9446e3462b6623bd2e645ffeb00b990c41
Jens Eraßmy <jens.erassmy@artcom.de>
2022-05-03 11:16:49 +0200

    Fix types

--- types/src/index.d.ts
+++ types/src/index.d.ts
@@ -1,25 +1,25 @@
 import { Props } from '@react-three/fiber/dist/declarations/src/web/Canvas';
 
 export interface ARCanvasProps extends Props {
-  arEnabled: boolean;
-  tracking: boolean;
-  patternRatio: number;
-  detectionMode: string;
-  cameraParametersUrl: string;
-  matrixCodeType: string;
-  sourceType: string;
-  onCameraStreamReady: () => void;
-  onCameraStreamError: () => void;
+  arEnabled?: boolean;
+  tracking?: boolean;
+  patternRatio?: number;
+  detectionMode?: string;
+  cameraParametersUrl?: string;
+  matrixCodeType?: string;
+  sourceType?: string;
+  onCameraStreamReady?: () => void;
+  onCameraStreamError?: () => void;
 }
 
 export interface ARMarkerProps {
   children: React.ReactNode;
   type: string;
-  barcodeValue: string;
-  patternUrl: string;
-  params: Object;
-  onMarkerFound: () => void;
-  onMarkerLost: () => void;
+  barcodeValue?: string;
+  patternUrl?: string;
+  params?: Object;
+  onMarkerFound?: () => void;
+  onMarkerLost?: () => void;
 }
 
 export declare function ARCanvas(props: ARCanvasProps): JSX.Element;
