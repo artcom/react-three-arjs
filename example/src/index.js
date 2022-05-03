60a1dd880914872874d5a33ace2445d809a895fa
Jens Eraßmy <jens.erassmy@artcom.de>
2022-05-03 12:35:42 +0200

    Adapt to r3f api changes

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,6 @@
 import { ARCanvas, ARMarker } from "@artcom/react-three-arjs"
 import React from "react"
 import { createRoot } from "react-dom/client"
-import { sRGBEncoding } from "three"
 
 function Box() {
   return (
@@ -14,13 +13,10 @@ function Box() {
 
 createRoot(document.getElementById("root")).render(
   <ARCanvas
-    gl={{ antialias: false, powerPreference: "default" }}
-    dpr={window.devicePixelRatio}
+    gl={{ antialias: false, powerPreference: "default", physicallyCorrectLights: true }}
     onCameraStreamReady={() => console.log("Camera stream ready")}
     onCameraStreamError={() => console.error("Camera stream error")}
     onCreated={({ gl }) => {
-      gl.outputEncoding = sRGBEncoding
-      gl.physicallyCorrectLights = true
       gl.setSize(window.innerWidth, window.innerHeight)
     }}>
     <ambientLight />
