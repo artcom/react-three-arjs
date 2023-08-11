afdab921e609d09a64f3b6596b8567f46e80346c
Riley Jones <rileyjones@Rileys-MacBook-Pro.local>
2023-08-11 11:17:35 +1000

    Added firebase

---
+++ app/src/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4gsG-7I4KDrw_wTYC_KzbkuFB6knF3aA",
+  authDomain: "gdsc-ar-hunt.firebaseapp.com",
+  databaseURL: "https://gdsc-ar-hunt-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gdsc-ar-hunt",
+  storageBucket: "gdsc-ar-hunt.appspot.com",
+  messagingSenderId: "596515515464",
+  appId: "1:596515515464:web:35dbf261da5b150fb9bc7d",
+  measurementId: "G-2EVL4289KF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth  = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+if (auth.currentUser) {
+    console.log("User logged in: " + auth.currentUser.displayName)
+}
+
+export {app, auth, provider, db}
--- app/src/pages/SignRules.js
+++ app/src/pages/SignRules.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import {auth, provider} from '../firebase';
+import { signInWithRedirect } from 'firebase/auth';
+
+const signIn = () => {
+  signInWithRedirect(auth, provider);
+}
 
 function SignRules() {
   return (
-    <div>SignRules</div>
+    <div>
+      <h1>Sign in Page</h1>
+      <button onClick={() => signIn()}>
+        Sign in 
+      </button>
+    </div>
   )
 }
 
